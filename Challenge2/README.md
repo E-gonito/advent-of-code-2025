a51944844a26864c9e9fffbd2c0199cad8ff5330
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2025-12-02 11:07:50 +0000

    feat(day2): Add part1 answer

---
+++ Challenge2/README.md
@@ -0,0 +1,10 @@
+f.split(",")
+invalidId = 0
+for each line in f:
+    line.split("-")
+    lower, upper
+    for x in range (lower, upper):
+        if magnitude of x % 2 == 0 (the number of digits in the number is even e.g 1000, 10, 100,000):
+            check if subnumber is repeated:
+                invalidID += ID
+return invalidID
--- Challenge2/main.py
+++ Challenge2/main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from challenge2!")
+def part1():
+    with open('puzzleinput.txt', 'r') as f:
+        invalid_id = 0
+        f = f.read().split(",")
+        for id_range in f:
+            lower = int(id_range.split("-")[0])
+            upper = int(id_range.split("-")[1])
+            for x in range(lower, upper + 1):
+                x_len = len(str(x))
+                if x_len % 2 == 0 and (str(x)[0:x_len//2] == str(x)[x_len//2:]) :
+                    invalid_id += x
+    print(invalid_id)
+    return invalid_id
+
+
 
 
 if __name__ == "__main__":
-    main()
+    part1()
