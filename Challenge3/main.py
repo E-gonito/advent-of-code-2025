c7a015ddc5bafde48ceb544276c79a6f8411f405
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2025-12-03 11:26:58 +0000

    feat(day3): Add part1 elegant solution

--- Challenge3/main.py
+++ Challenge3/main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from challenge3!")
+def part1():
+    with open('puzzleinput.txt', 'r') as f:
+        joltage_sum = 0
+        for line in f.read().strip().split('\n'):
+            first_pos = line.index(max(line[:-1]))
+            second_pos = line.index(max(line[first_pos+1:]))
+            joltage_sum += int(line[first_pos]) * 10 + int(line[second_pos])
+        print(joltage_sum)
+        
+def part2():
+    with open('puzzleinput.txt', 'r') as f:
+        joltage_sum = 0
+        for line in f.read().strip().split('\n'):
+            
+
+        
 
 
 if __name__ == "__main__":
-    main()
+    part1()
