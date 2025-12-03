1b8d28e1b21e427bf8ff08960d09c7d56b0c781c
E-gonito <errolson.gonito.2019@live.rhul.ac.uk>
2025-12-03 19:24:38 +0000

    feat(day3): Add part2 answer

--- Challenge3/main.py
+++ Challenge3/main.py
@@ -8,13 +8,28 @@ def part1():
         print(joltage_sum)
         
 def part2():
+    RESULT_LENGTH = 12
     with open('puzzleinput.txt', 'r') as f:
         joltage_sum = 0
         for line in f.read().strip().split('\n'):
+            result = ""
+            current_pos = -1 
+            for current_digit in range(1, 13):
+                start = current_pos + 1
+                end = (len(line) - 1) - (RESULT_LENGTH - current_digit)
+                search_window = line[start:end+1]
+                max_digit = max(search_window)
+                max_digit_pos = search_window.index(max_digit)
+                current_pos = start + max_digit_pos
+                result += line[current_pos]
+            joltage_sum += int(result)
+        print(joltage_sum)
+            
+            
             
 
         
 
 
 if __name__ == "__main__":
-    part1()
+    part2()
